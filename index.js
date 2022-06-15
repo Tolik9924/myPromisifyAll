4e319cec1b220f79040dc67ebeaff14714a02737
Tolik9924 <76564089+Tolik9924@users.noreply.github.com>
2022-06-15 18:15:10 +0300

    re

---
+++ index.js
@@ -0,0 +1,53 @@
+const { promisify } = require('util');
+
+const myPromisify = (fn) => {
+    return (...args) => {
+        return new Promise((resolve, reject) => {
+            function customCallback(err, ...results) {
+                if(err) {
+                    return reject(err);
+                } else {
+                    return resolve(results.length === 1 ? results[0] : results);
+                }
+            }
+            args.push(customCallback);
+            fn.call(this, ...args);
+        })
+    }
+}
+
+const myPromisifyAll = (obj) => {
+    let getObjFuncPromise;
+
+   arrObjFuncs = Object.values(obj);
+   arrObjFuncs.forEach((item) => {
+        getObjFuncPromise = myPromisify(item)
+        getObjFuncPromise(3, 2).then(result => {
+            console.log(result);
+        })
+   })
+}
+
+let obj = {
+    func1: (num1, num2, callback) => {
+        if (!num1 || !num2) {
+            return callback(new Error("Missing arguments"), null);
+        }
+    
+        const sum = num1 + num2;
+        const message = `Sum is ${sum}`;
+        return callback(null, sum, message);
+    },
+
+    func2: (num1, num2, callback) => {
+        if (!num1 || !num2) {
+            return callback(new Error("Missing arguments"), null);
+        }
+    
+        const minus = num1 - num2;
+        const message = `Minus is ${minus}`;
+        return callback(null, minus, message);
+    } 
+}
+
+myPromisifyAll(obj);
